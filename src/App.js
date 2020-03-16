d44738709ed3c19f415c66e1bd2fd91f98e4863d
Alexinder Virdee <alex.virdee08@gmail.com>
2020-03-16 09:57:55 -0400

    Skeleton boxes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Axios from 'axios';
+import './App.css';
 
 export default class App extends React.Component {
   state = {
     confirmed: 0,
     recovered: 0,
     deaths: 0
-  }
+  };
 
   componentDidMount() {
     this.getData();
@@ -18,12 +19,31 @@ export default class App extends React.Component {
     this.setState({
       confirmed: res.data.confirmed.value,
       recovered: res.data.recovered.value,
-      deaths: res.data.deaths.value,
+      deaths: res.data.deaths.value
     });
-
   }
 
   render() {
-    return (<div>App</div>)
+    return (
+      <div className="container">
+         <h1>COVID-19 Stats</h1>
+        <div className="row">
+        <div className="flex">
+        <div className="box confirmed">
+            <h4>Confirmed Cases</h4>
+            <h4>{this.state.confirmed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</h4>
+          </div>
+          <div className="box recovered">
+          <h4>Recovered Cases</h4>
+            <h4>{this.state.recovered.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</h4>
+          </div>
+          <div className="box deaths">
+          <h4>Deaths</h4>
+            <h4>{this.state.deaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</h4>
+          </div>
+        </div>
+        </div>
+      </div>
+    );
   }
 }
