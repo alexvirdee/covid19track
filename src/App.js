fcd31c4485682bf8dc2d74427b080389d95f6d95
Alexinder Virdee <alex.virdee08@gmail.com>
2020-03-16 09:37:32 -0400

    Populate state with information from api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    confirmed: 0,
+    recovered: 0,
+    deaths: 0
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  async getData() {
+    const res = await Axios.get('https://covid19.mathdro.id/api');
+
+    this.setState({
+      confirmed: res.data.confirmed.value,
+      recovered: res.data.recovered.value,
+      deaths: res.data.deaths.value,
+    });
+
+  }
+
+  render() {
+    return (<div>App</div>)
+  }
 }
-
-export default App;
